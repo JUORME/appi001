8c0e9db0a285757adba80934c5bfd5c06fc264fa
junior <gaminjuniorortiz@gmail.com>
2021-02-24 23:58:36 -0500

    iniciando sliderInput

--- appR/app/app.r
+++ appR/app/app.r
@@ -3,3 +3,15 @@ ent <- 1
 options(encoding = "utf-8")
 options(shiny.maxRequestSize = 30*1024^2)
 options(warn=-1)
+
+library(shiny)
+
+shinyApp(
+	ui = fluidPage(
+		sliderInput(inputId = "num",
+			label = "Choose Number",
+			value = 10, min = 1, max = 100),
+		plotOutput("hist")
+	),	
+	server <- function(input, output){}
+)	 
---
+++ appR/exec.r
@@ -0,0 +1,12 @@
+ent <- 1
+if(ent == 1){
+	pathglo <- "D:/github/appi001/appR"
+}else {
+	pathglo <- "/srv/shiny-server/appi001/appR"
+}
+
+setwd(pathglo)
+
+library(shiny)
+
+runApp("app", host="0.0.0.0", port=8100)
